e377c3e7bf0228bc35f72d09af72f5969a1b934d
greek0x0 <61706f6c6c6f@protonmail.com>
2022-05-19 13:50:36 +0100

    script fix

--- install.sh
+++ install.sh
@@ -11,13 +11,12 @@ sudo pip install keep #needs to run as sudo
 GIT=https://github.com/greek0x0
 mkdir $HOME/Repo
 REPO=$HOME/Repo
-git clone $GIT/vimrc $REPO
-git clone $GIT/dits $REPO
-git clone $GIT/aliasrc $REPO
-git clone $GIT/ammo $REPO
-git clone $GIT/x86-64-assembly $REPO
-git clone $GIT/x86_64-basic-bootsector $REPO
-
+git clone $GIT/vimrc $REPO/vimrc
+git clone $GIT/dots $REPO/dots
+git clone $GIT/aliasrc $REPO/aliasrc
+git clone $GIT/ammo $REPO/ammo
+git clone $GIT/x86-64-assembly $REPO/x86-64-assembly
+git clone $GIT/x86_64-basic-bootsector $REPO/x86_64-basic-bootsector
 
 
 
