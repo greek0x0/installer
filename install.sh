ac929834488c8b154bcc8b5adf84f54440b44878
greek0x0 <61706f6c6c6f@protonmail.com>
2022-05-24 10:31:21 +0100

    terminator config

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ sudo mv tmux.conf /etc
 rm -r tmux.conf
 #pwncat 
 sudo docker build -t pwncat $REPO/pwncat/
+#pwncat config
 cat << EOF >> /tmp/pwncatrc
 set -g lhost "127.0.0.1"
 set -g prefix c-k
@@ -76,3 +77,42 @@ chmod +x $REPO/aliasrc/setup.sh
 sh $REPO/aliasrc/setup.sh
 mkdir $HOME/.keep
 cp $REPO/ammo/commands.json $HOME/.keep/
+
+
+cat << EOF >> /tmp/config
+[global_config]
+  window_state = fullscreen
+  scroll_tabbar = True
+  homogeneous_tabbar = False
+  title_transmit_bg_color = "#000000"
+  title_receive_bg_color = "#d41919"
+  title_inactive_fg_color = "#ffffff"
+  title_inactive_bg_color = "#000000"
+  enabled_plugins = LaunchpadBugURLHandler, LaunchpadCodeURLHandler, APTURLHandler, Logger
+[keybindings]
+  next_tab = <Alt>a
+  prev_tab = <Alt>s
+[profiles]
+  [[default]]
+    background_darkness = 0.96
+    background_type = transparent
+    cursor_color = "#0bff2b"
+    cursor_color_fg = False
+    font = Fira Code Semi-Bold 13
+    foreground_color = "#0bff2b"
+    show_titlebar = False
+    scrollbar_position = hidden
+    scrollback_infinite = True
+    palette = "#073642:#dc322f:#859900:#b58900:#268bd2:#d33682:#2aa198:#eee8d5:#002b36:#cb4b16:#586e75:#657b83:#839496:#6c71c4:#93a1a1:#fdf6e3"
+    bold_is_bright = True
+[layouts]
+  [[default]]
+    [[[window0]]]
+      type = Window
+      parent = ""
+    [[[child1]]]
+      type = Terminal
+      parent = window0
+[plugins]
+EOF 
+mv /tmp/config $HOME/.config/terminator/
